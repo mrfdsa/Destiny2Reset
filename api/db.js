d1d76ca567162af29b86bec6a94f1d6c57f47db4
mrfdsa <43702887+mrfdsa@users.noreply.github.com>
2023-10-23 19:27:52 +0900

    Add files via upload

--- api/db.js
+++ api/db.js
@@ -1,72 +1,112 @@
 import sqlite3 from 'sqlite3';
 import cors from 'cors';
 import express from 'express';
-import { connect, config } from './bungie.js';
+// import { connect, config } from './bungie.js';
 import dotenv from 'dotenv';
+import test from './test.json' assert {type : 'json'};
 dotenv.config({ path: '../.env' });
 
 //전역변수
-const vgList = ["vgHero", "vgLegend", 'vgMaster', "vgGrandMaster"];
-let vgInfo = {};
-let hashes = {};
-let finalInfo = {};
+const vgList = ["vgHero", "vgLegend", 'vgMaster', "vgGrandMaster"]
+let url = {
+    'bgImage' : '',
+    'icType' : '',
+    'icItem' : ''
+}
+let vgInfo = {}
+let vgHashes = {}
+let vgModifier = {};
+let vgDesc = {}
+let modifierRipped = {};
+//테스트용 객체
+
 //server
 //express & cors & json parse
 const app = express();
-const port = process.env.PROT || 8080;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    const result = new Promise((resolve, reject) => {
-        getInfo();
-        for (let i = 0; i < hashes.length; i++) {
-            getSetHash(hashes[i]);
-        };
-        resolve(res.send(finalInfo));
-    });
+//미들웨어
+let middleWare = function(req,res,next) {
+    getInfo();
+    getQuery();
+    next();
+}
+app.use(middleWare);
+app.get('/test', (req, res,error) => {
+    res.send(modifierRipped);
 });
 
 app.listen(port, () => {
     console.log(`running on ${port}`);
 });
 
-
+//주간 공격전 해쉬 추출
+//그리고 해쉬변환
+//나중에 연결할때 async 붙일 것
 async function getInfo() {
-    const result = await connect(config);
-    const weeklyReset = result['data']['Response']['2029743966']['activities'];
+    console.log('1');
+    // const result = await connect(config);
+    // const weeklyReset = await result['data']['Response']['2029743966']['activities'];
+    const weeklyReset = test['Response']['2029743966']['activities'];
     for (let i = 0; i < vgList.length; i++) {
         vgInfo[vgList[i]] = weeklyReset[i];
+        vgHashes[i] = vgInfo[vgList[i]]['activityHash'] >> 32;
     }
-    return getSetHash();
+    for (const keys of Object.keys(vgInfo)) {
+        vgModifier[keys] = vgInfo[keys]['modifierHashes'];
+    }
+    return {vgHashes,vgModifier};
 }
 
-//hash 변환
-async function getSetHash() {
-    for (let i = 0; i < vgList.length; i++) {
-        hashes[i] = vgInfo[vgList[i]]['activityHash'] >> 32;
-    }
-    return hashes;
-}
 
 //DB연결, 쿼리 결과 받아오기
-async function getQuery(e) {
-    const conDB = {
-        dbLoc: "../db/content.db",
-        query: 'SELECT json FROM DestinyActivityDefinition WHERE id=?'
-    }
-    let db = new sqlite3.Database(conDB.dbLoc, sqlite3.OPEN_READONLY);
-    db.all(conDB.query, [e], (error, result) => {
-        finalInfo = result;
-        console.log(finalInfo);
-    })
-    db.close();
-    return finalInfo;
+const conDB = {
+    'dbLoc' : '../db/content.db',
+    'modifierQuery' : 'select json from DestinyActivityModifierDefinition where id=?',
+    'activityQuery' : 'select json from DestinyActivityDefinition where id=?',
+    'rewardQuery' : 'select json from Dsetin'
 }
 
+function getQuery() {
+    console.log('2');
+    //mysql3
+    const db = new sqlite3.Database(conDB['dbLoc'], sqlite3.OPEN_READONLY,(err)=> {
+        try {
+            console.log('connnected to DB1');
+        }catch(error) {
+            console.log(err);
+        }
+     });
+    for(let i=0; i<vgList.length; i++) {
+            db.get(conDB['activityQuery'], vgHashes[i], (error, result) => {
+                let tuneResult = JSON.parse(result['json']);
+                try {
+                    vgDesc[vgList[i]] = {
+                        'displayProperties' : tuneResult['displayProperties'],
+                        'reward' : tuneResult['rewards'][0]['rewardItems']
+                    }
+                }
+                catch(error) {
+                    console.log('error occured, try again.');
+                }
+        })  
+    }
+    for(const values of vgModifier['vgGrandMaster']) {
+        let hash = values >> 32;
+        db.get(conDB.modifierQuery,[hash],(error,result)=> {
+            let title = JSON.parse(result['json'])['displayProperties']['name']
+            modifierRipped[title] = JSON.parse(result['json'])['displayProperties']
+        })
+    }
+    db.close((err)=> {
+        if(err) {
+            console.log(err);
+        }
+        console.log('disconnected')
+    });
+    return {vgDesc,modifierRipped};
+}
 
-//모디파이어 해쉬테이블
-//근데 이거까진 만들어야 하나???? 모르겠다 진짜
-// async function vgHashTb() {
-//     for(let i=0; i<Object.keys(hashes); i++) {
-
-//     }
-// }
+function getReward() {
+    db.get(conDB['rewardQuery'],[])
+}
