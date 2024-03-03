f612dd870da23a77e5c79dcc02f9beba990dc07d
mrfdsa <43702887+mrfdsa@users.noreply.github.com>
2024-03-03 20:13:27 +0900

    Update App.js
    react-query 적용

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-import './App.css';
 import Header from './comp/header.js';
+import Raid from './comp/raid.js';
+import Vanguard from './comp/vanguard.js';
+import Lost from './comp/lost.js';
 import Main from './comp/main.js';
-import Footer from './comp/footer.js';
-function App() {
 
-  return (
-    <div id="appWrapper">
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
-  );
+import {QueryClientProvider,QueryClient} from '@tanstack/react-query';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+function App() {
+  const queryClient = new QueryClient();
+  const route = createBrowserRouter([
+  {
+    path:'/',
+    element: <Header />,
+    children: [
+      {
+        path:'/vanguard',
+        element: <Vanguard />
+      },
+      {
+        path: '/raid',
+        element: <Raid />
+      },
+      {
+        path:'/lost',
+        element: <Lost />,
+      },
+      {
+        path:'/',
+        element: <Main />
+      }
+    ]
+  },
+  
+  ])
+  return(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={route} />
+    </QueryClientProvider>
+  )
+  
 }
 
 export default App;
