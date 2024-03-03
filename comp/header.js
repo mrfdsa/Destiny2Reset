16cd21d907045d2646ec4b5109f01cc6aac65e54
mrfdsa <43702887+mrfdsa@users.noreply.github.com>
2024-03-03 20:15:42 +0900

    Update header.js
    - 헤더 디자인 & 요소 추가

--- comp/header.js
+++ comp/header.js
@@ -1,11 +1,25 @@
-import '../style/header.css'
-
+import '../style/header.css';
+import {Outlet,Link} from 'react-router-dom'
 function Header() {
+    //set nav list & navState
+    const navList = ['vanguard','Lost','Raid'];
+    //show nested informations
     return(
-        <div id="headerWrapper" className='headerFlex'>
-            <span id="title">데스티니 정보뷰어</span>
-        </div>
+        <>
+            <header id="headerWrapper" className='headerFlex'>
+                <Link id="title" className='font navHyper' to='/'>데스티니 정보뷰어</Link>
+                <ul id="navigation">
+                    {navList.map((val,index) => {
+                        return(
+                            <li key={index} className='font navList'><Link to={val} key={index} className='navHyper'>{val}</Link></li>
+                        )
+                    })}
+                </ul>
+            </header>
+            <div id="wrpContent">
+               <Outlet />
+            </div>
+        </>
     )
 }
-
 export default Header;
