19738f0c4281b989642a8f69b09c097f08422062
mrfdsa <43702887+mrfdsa@users.noreply.github.com>
2024-03-03 20:12:53 +0900

    2024 3 3
    - fakeJSON 사용해서 블로그 글 페이지네이션 구현중

--- comp/main.js
+++ comp/main.js
@@ -1,191 +1,105 @@
-import axios from 'axios';
 import '../style/main.css';
-// import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import {useQuery} from '@tanstack/react-query';
-
-//축약어 리스트
-//vg : vanGuard(공격전)
-//md : Modifier(모디파이어)
-//cmp : component(컴포넌트)
-//queryClient component;
-
-
-//고려사항
-//api 숨기기
-//전역변수 설계
-
-//전역변수
-const config = {
-    imgUrl: 'https://bungie.net',
-    url: 'http://localhost:8080/test',
-    levels: ['vgHero','vgLegend','vgMaster','vgGrandMaster']
-}
-
-//reactQuery getQuery
-// function getQuery() {
-//     const {data,isLoading} = useQuery({
-//         queryKey: ['dataResult'],
-//         queryFn:async()=> {
-//             const result = await axios.get(config['url']);
-//             return result['data'];
-//         }ww
-//     });
-//     return {data,isLoading};
-// }
+import axios from 'axios';
+import { useState } from 'react';
+export default function Main() {
+    /**넣을것들 */
+    /** - 현재 시즌 */
+    /** - 현재 남은시간*/
+    /** 넣고싶은 것들*/
+    /** - 번지 블로그 RSS*/
+    // https://deepsight.gg/manifest/DeepsightDropTableDefinition.json
+    //test
+    //test2
+    //Axios config
+    const curSeason = '소원의 시즌';
+    const curDate = new Date();
+    const date = `${curDate.getFullYear()}-${curDate.getMonth()}-${curDate.getDay()}`;
+    const seasonStart = new Date('2023-11-29');
+    const seasonEnd = new Date('2024-06-05');
+    const remain = setStr(Math.ceil((seasonEnd-seasonStart)/3600/1000/24));
+    const config = {
+        method: 'get',
+        test: 'https://dummyjson.com/products',
+    };
+    //날짜 문자열로 바꾸는 함수
+    //ㅅㅂ 더 나은 방법이 있을텐데
+    function setStr(date) {
+        return JSON.stringify(date);
+    }
 
 
-function Main() {
-    //reactQuery & state
-    //mainComp
+    //일단 데이터 불러와야 야하는거
+    //1. deepsight.gg - https://deepsight.gg/manifest/DeepsightDropTableDefinition.json
+    //2. bungieAPI news RSS - 
     const {data,isLoading,error} = useQuery({
-        queryKey: ['dataResult'],
+        queryKey:['data'],
         queryFn:async()=> {
             try{
-                const result = await axios.get(config['url']);
-                console.log(result['data']);
-                return result['data'];
-            }catch(error) {
+                const data = await axios.get(config['test']);
+                return data['data'];
+            }catch(err) {
                 console.log(error);
             }
         }
     });
-    //vanguard Component
-    //map() 함수 혹은 비슷한 함수들을 사용해 모디파이어 & 보상 아이콘 이미지 출력
-    //같이 사용될 함수는 일단 하나의 함수로 묶어버리기
-    // function getRender(data,element) {
-    //     const renderValue = data;
-    //     let output = [];
-    //     for(const [level,desc] of Object.entries(data)) {
-    //         for(cosnt [index,value] of Object.entries(desc)) {
-    //             output.push(<element key={index} alt={desc['name']} src={config['imgUrl']+value['icon']} className='icModifier flex' />)
-    //         }
-    //     }
-    //     return output;
-    // }
-
-    // 보상 & 모디파이어 작성한 코드
-    // function CmpModifier() {
-    //     const vgModifier = data['modifier']['vgHero'];
-    //     let output=[];
-    //     for(const [index,value] of Object.entries(vgModifier)) {
-    //         if(value['name']) {
-    //         output.push(<img key={index} alt={value['name']} src={config['imgUrl']+value['icon']} className='icModifier flex'/>)
-    //     }}
-    //     return(
-    //         <div id="wrpModifier" className="border">
-    //             {output}
-    //         </div>
-    //     )
-    // }
-
-    // function CmpReward() {
-    //     //난이도별 모디파이어 추출
-    //     const vgReward = data['rewards']['vgHero'];
-    //     let output=[];
-    //     for(const [index,value] of Object.entries(vgReward)) {
-    //         if(value['name']) {
-    //             output.push(<img key={index} alt={value['name']} src={config['imgUrl']+value['icon']} className='icReward flex'/>)
-    //         }
-    //     }
-    //     return (
-    //         <div id="wrpReward" className="border">
-    //             {output}       
-    //         </div>
-    //     )
-    // }
-    //dataSet: data 종류(modifier,level,etc...)
-    //status: style 지정용 class이름(icModifier,icReward)
-    //level: 공격전 난이도(vgHero,vgLevend,vgMaster,vgGrandMaster)
-      
-    //subComp - crucible(예정)
-    //subComp - raid(예정)
     if(isLoading) {
         return(
-            <div id="wrpMain" className="mainFlex">
-                <h1 className="mapTitle">Loading...</h1>
-            </div>
-        );
-        //고려사항
-        //로딩시간이 길어진다면?
-        //로딩 중 로딩게이지 말고 뭘 더 보여줘야하나?
+        <section id='main_loading'>
+            <h2 className='font'>Loading...</h2>
+        </section>)
     }
-
     if(error) {
-        return (
-            <div id="wrpMain" className="mainFlex bgImg">
-                <h1 className="mapTitle">{error}</h1>
-            </div>
-        )
-        //고려사항
-        //에러케이스 세분화
-        //http 에러코드별 분류
-        //겉은 에러메세지, 에러정보를 서버에 전송해야 한다.
+        console.log(error.message);
     }
-
     if(data) {
-        const url=config['bgUrl']+data['style']['bgImg'];
-        const levels = Object.keys(data['modifier']).map((val)=>val);
-        function RenderComponent(dataSet,level) {
-            const result = data[dataSet][level];
-            let elId='';
-            let output=[];
-            for(const [index,value] of Object.entries(result)) {
-                if(result[index]['desc'] !== '') {
-                    output.push(<img key={index} alt={value['name']} src={config['imgUrl']+value['icon']} className='ic' />)
-                }
-            }
-            switch(dataSet) {
-                case 'modifier':
-                    elId='wrpModifier';
-                    return(
-                        <div id={elId} className='border mainGrid' >
-                            {output}
-                        </div>
-                    );
-                case 'rewards':
-                    elId='wrpRewards'
-                    return(
-                        <div id={elId} className='border flexRow'>
-                            {output}
-                        </div>
-                    );
-            }
-        } 
-
-        if(data) {
-        const levels = Object.keys(data['modifier']).map((val)=>val);
-
-        function RenderComponent(dataSet,level) {
-            let result = data[dataSet][level];
-            let output = [];
-            for(const [index,value] of Object.entries(result)) {
-                if(result[index]['desc'] !== '') {
-                    output.push(<img key={index} alt={value['name']} src={config['imgUrl']+value['icon']} className='ic' />);
-                }
-            }
-            return(
-                <div className={`border mainGrid wrp_${dataSet}`} >
-                    {output}
-                </div>
-            );
-        } 
-        //render render component, wtf??????
+        const products = data['products'];
         return(
-            <div id="wrpMain" className="mainFlexRow bgImg">
-                {levels.map((level)=>
-                    <div className="wrpInfo mainFlex" key={levels.indexOf(level)}>
-                        {config['dataSet'].map((data)=> RenderComponent(data,level))}
-                    </div>
-                )}
-            </div>
-        )
-    }
+        <section id='main_wrapper' className='main_grid'>
+            <article id='main_title' className='main_flex'>
+                <p>{curSeason}</p>
+            </article>
+            <article id='main_season' className='main_flex'>
+                <p>시즌 시작 : {setStr(seasonStart)}</p>
+                <p>시즌 종료 : {setStr(seasonEnd)}</p>
+                <p>시즌 종료까지 {remain}일</p>
+            </article>
+            <article id='main_blog' className='main_flex'>
+            <button type='button' onClick={btnAction()}>Prev</button>
+            <button type='button' onClick={btnAction()}>Next</button>
+            {
+                Object.values(products).map((val,index)=> {
+                    if(index > 3) return;
+                    return (
+                    <>
+                        <BlogPost index={index} data={val}/>
+                    </>)
+                })
+            }
+            </article>
+        </section>
+        );
+    }    
+}
 
-    return (
-        <div id="wrpMain" className="flexRow bgImg">
-            <h1>data : null</h1>
-        </div>
-    )
-    
-};
-export default Main;
+function btnAction(val) {
+    //prev
+    //next
+    return val;
+}
+
+
+function BlogPost(val) {
+    const {id,title,thumbnail,description} = val['data'];
+    return(
+    <>
+        <a key={id} herf="#" className="main_article main_flex">
+            <img  className='main_img' src={thumbnail} alt={title}/>
+            <article  className='main_flex'>
+                <span  className='main_desc font'>{description}</span>
+                <span  className='main_title font'>{title}</span>
+            </article>
+        </a>
+    </>
+    );
+}
